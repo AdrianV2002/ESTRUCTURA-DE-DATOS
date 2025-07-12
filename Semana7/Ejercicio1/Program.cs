a9dbf1e9fb07dea21ab40381acb33494a0d1ed67
AdrianV2002 <aj.villegasc@uea.edu.ec>
2025-07-12 15:45:35 -0500

    Semana#7

---
+++ Semana7/Ejercicio1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class BalanceChecker
+{
+    /// <summary>
+    /// Verifica si los paréntesis, corchetes y llaves están balanceados en una expresión.
+    /// </summary>
+    static bool EstaBalanceado(string expresion)
+    {
+        Stack<char> pila = new Stack<char>();
+
+        foreach (char caracter in expresion)
+        {
+            if (caracter == '(' || caracter == '{' || caracter == '[')
+            {
+                pila.Push(caracter);
+            }
+            else if (caracter == ')' || caracter == '}' || caracter == ']')
+            {
+                if (pila.Count == 0) return false;
+
+                char tope = pila.Pop();
+                if ((caracter == ')' && tope != '(') ||
+                    (caracter == '}' && tope != '{') ||
+                    (caracter == ']' && tope != '['))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return pila.Count == 0;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Ingrese una expresión matemática:");
+        string expresion = Console.ReadLine();
+
+        if (EstaBalanceado(expresion))
+            Console.WriteLine("Fórmula balanceada.");
+        else
+            Console.WriteLine("Fórmula no balanceada.");
+    }
+}
---
+++ Semana7/Ejercicio2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    // Esta función mueve los discos usando recursividad
+    static void MoverDiscos(int n, Stack<int> origen, Stack<int> destino, Stack<int> auxiliar,
+                            string nombreOrigen, string nombreDestino, string nombreAuxiliar)
+    {
+        if (n == 1)
+        {
+            int disco = origen.Pop();
+            destino.Push(disco);
+            Console.WriteLine($"Mover disco {disco} de {nombreOrigen} a {nombreDestino}");
+        }
+        else
+        {
+            MoverDiscos(n - 1, origen, auxiliar, destino, nombreOrigen, nombreAuxiliar, nombreDestino);
+
+            int disco = origen.Pop();
+            destino.Push(disco);
+            Console.WriteLine($"Mover disco {disco} de {nombreOrigen} a {nombreDestino}");
+
+            MoverDiscos(n - 1, auxiliar, destino, origen, nombreAuxiliar, nombreDestino, nombreOrigen);
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Ingrese el número de discos:");
+        int n = int.Parse(Console.ReadLine());
+
+        Stack<int> torreA = new Stack<int>();
+        Stack<int> torreB = new Stack<int>();
+        Stack<int> torreC = new Stack<int>();
+
+        for (int i = n; i >= 1; i--)
+        {
+            torreA.Push(i);
+        }
+
+        Console.WriteLine("\nMovimientos necesarios para resolver Torres de Hanoi:");
+        MoverDiscos(n, torreA, torreC, torreB, "Torre A", "Torre C", "Torre B");
+
+        Console.WriteLine("\nTodos los discos han sido movidos a la Torre C.");
+    }
+}
